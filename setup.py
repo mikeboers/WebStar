82b8651c9eed0c19224c8a7b53a0bedae81337a3
Mike Boers <github@mikeboers.com>
2011-06-01 20:06:02 -0700

    Add a setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup, find_packages
+
+setup(
+
+    name = "WebStar",
+    version = "0.1b",
+        
+    author="Mike Boers",
+    author_email="webstar@mikeboers.com",
+    license="BSD-3"
+)
